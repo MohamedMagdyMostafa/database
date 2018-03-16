d00245680cea1431553d5a464e9285b43c34f9bc
MohamedMagdyMostafa <mail@mohamed-magdy.com>
2018-03-16 05:07:52 +0200

    Create vendor.php

---
+++ vendor.php
@@ -0,0 +1,57 @@
+<?php
+//////////////////////////////////////////////////////////////////
+
+
+	/*******       DATABASE CONNECTION STRING            *******/
+
+    	
+
+		//set database host & database name
+		define('DSN',"mysql:host=localhost; dbname=YOUR_DB_NAME");         
+		//set database user              
+		define('DB_USER',"root"); 
+		//set database password                         						   
+		define('DB_PASS',"");           
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+	/*******           OOP CLASSES INCLUDES             ********/
+
+		include 'db_class.php';
+		
+
+
+//////////////////////////////////////////////////////////////////
+
+
+	/*******              CREATE OBJECTS                ********/
+
+
+		$db_class = new db_class;
+
+
+//////////////////////////////////////////////////////////////////
+
+  
+
+//         ||          ||
+//         ||\\      //||
+//         || \\    // ||
+//         ||  \\  //  ||			PHP      	UI/UX
+//         ||   \\//   ||       DEVELOPER   Specialest
+//         ||          ||        
+//         || mohamed  ||     all rights reserved 2017©	
+//         ||  magdy   ||    	mail@mohamed-magdy.com
+//         || mostafa  ||		 
+//         ||          ||
+
+
+
+//////////////////////////////////////////////////////////////////
+
+	
+
+ ?>
